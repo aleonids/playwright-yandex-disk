71bcfc715534def9f0aa72903b8091cc170ac03e
Leanid Aleinikau <laleynikov@kameleoon.com>
2023-04-23 22:11:14 +0300

    fix: jenkins file

--- src/core/global-setup.ts
+++ src/core/global-setup.ts
@@ -4,7 +4,7 @@ import { login } from "./login";
 async function globalSetup(config: FullConfig): Promise<void> {
   const username = process.env.LOGIN_NAME ?? "";
   const password = process.env.PASSWORD ?? "";
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
 
   await login({ page, username, password });
