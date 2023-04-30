d7686bf1dd352879892db22a16ea58aba0234186
Leanid Aleinikau <laleynikov@kameleoon.com>
2023-04-30 16:09:40 +0300

    feat: add report

--- scripts/clear_report.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# if report and results exist, delete them
-if [ -d "allure-results" ]; then rm -Rf allure-results; fi
-if [ -d "allure-report" ]; then rm -Rf allure-report; fi
-
--- src/core/types.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from "@playwright/test";
-
-export type TypeLogin = {
-  page: Page;
-  username: string;
-  password: string;
-};
